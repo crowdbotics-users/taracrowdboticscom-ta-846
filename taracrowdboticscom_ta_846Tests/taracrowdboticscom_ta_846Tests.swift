e06c63b855b9b00d0abe50c73249c8280a45aff8
Crowdbotics <team@crowdbotics.com>
2019-01-23 13:15:07 +0000

    initial commit

---
+++ taracrowdboticscom_ta_846Tests/taracrowdboticscom_ta_846Tests.swift
@@ -0,0 +1,34 @@
+//
+//  taracrowdboticscom_ta_846Tests.swift
+//  taracrowdboticscom_ta_846Tests
+//
+//  Created by Dan on 10/23/18.
+//  Copyright © 2018 Crowdbotics. All rights reserved.
+//
+
+import XCTest
+@testable import taracrowdboticscom_ta_846
+
+class taracrowdboticscom_ta_846Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
